81825466cbd7416a9cb71ddacd133c50c0ebdcca
good-riverdeer <riverdeer.youn@gmail.com>
2024-08-09 17:28:58 +0900

    add method to get networkX graph

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
-from utils import smiles_to_fingerprint
+from utils import smiles_to_fingerprint, smiles_to_graph
 
 
 COLUMNS = [
@@ -15,6 +15,9 @@ if __name__ == '__main__':
     chembl_data = pd.read_csv('../data/train.csv')
     df = chembl_data[COLUMNS]
     df["fingerprint"] = df["Smiles"].apply(smiles_to_fingerprint)
+
+    graph_0 = smiles_to_graph(df.loc[0, "Smiles"])
+    df["graph"] = df["Smiles"].apply(smiles_to_graph)
 
     features = np.stack(df["fingerprint"].values)
     target = df["pIC50"].values
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,47 @@
 import numpy as np
+import networkx as nx
 
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
 
+fp_gen = AllChem.GetMorganGenerator(radius=2, fpSize=1024)
+
+
 def smiles_to_fingerprint(smiles):
     mol = Chem.MolFromSmiles(smiles)
     if mol is not None:
-        fp = AllChem.GetMorganFingerprintAsBitVect(mol, 2, nBits=1024)
+        # fp = AllChem.GetMorganFingerprintAsBitVect(mol, 2, nBits=1024)
+        # return np.array(fp)
+        fp = fp_gen.GetFingerprint(mol)
         return np.array(fp)
     else:
         return np.zeros((1024,))
+
+
+def smiles_to_graph(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+
+    G = nx.Graph()
+    for atom in mol.GetAtoms():
+        G.add_node(
+            atom.GetIdx(),
+            labels=atom.GetSymbol(),
+            atomic_num=atom.GetAtomicNum(),
+            formal_charge=atom.GetFormalCharge(),
+            chiral_tag=atom.GetChiralTag(),
+            hybridization=atom.GetHybridization(),
+            num_explicit_hs=atom.GetNumExplicitHs(),
+            is_aromatic=atom.GetIsAromatic(),
+        )
+    for bond in mol.GetBonds():
+        G.add_edge(
+            bond.GetBeginAtomIdx(),
+            bond.GetEndAtomIdx(),
+            bond_type=bond.GetBondType(),
+            bond_type_value=bond.GetBondTypeAsDouble(),
+        )
+    return G
 
 
 def pIC50_to_IC50(pic50_values):
